54fe9de0a264bf17ffa42bcbe1a879d8d4bbcba7
Lumindia <hemmings.dan@gmail.com>
2016-10-15 13:32:52 -0400

    Spreading code is working.

--- src/main/java/com/lumindia/modthing/block/BlockTutorialBlock.java
+++ src/main/java/com/lumindia/modthing/block/BlockTutorialBlock.java
@@ -5,11 +5,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Random;
+
+import static java.lang.System.out;
 
 
 /**
@@ -28,14 +31,28 @@ public class BlockTutorialBlock extends Block {
         this.setResistance(10F);
         this.setSoundType(SoundType.SLIME);
         this.setLightLevel(1F);
+        this.setTickRandomly(true);
     }
 
     @Override
     public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-        spread = rand.nextInt(10000);
+        spread = rand.nextInt(8);
         if (spread <= 5) {
-            if (worldIn.getBlockState(pos).getMaterial() != blockMaterial.AIR) {
+            for (int i = 0; i < 5; ++i){
+                BlockPos blockpos = pos.add(rand.nextInt(2) - 1, rand.nextInt(2) - 1, rand.nextInt(2) - 1);
 
+                if (blockpos.getY() >= 0 && blockpos.getY() < 256 && !worldIn.isBlockLoaded(blockpos)){
+                    return;
+                }
+
+                IBlockState iBlockState = worldIn.getBlockState(blockpos);
+
+                if (iBlockState.getMaterial() != blockMaterial.AIR) {
+
+                    worldIn.setBlockState(blockpos, Blocks.GRASS.getDefaultState());
+                    System.out.println("Block Converted!");
+
+                }
             }
         }
 
