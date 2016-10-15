e7e8a673674724693aab7a96fdaa0d262ecd203a
Lumindia <hemmings.dan@gmail.com>
2016-10-15 12:41:09 -0400

    first attempt at making Grey Goo-type block thing

--- src/main/java/com/lumindia/modthing/block/BlockTutorialBlock.java
+++ src/main/java/com/lumindia/modthing/block/BlockTutorialBlock.java
@@ -4,6 +4,12 @@ import com.lumindia.modthing.ModThing;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 
 /**
@@ -11,12 +17,30 @@ import net.minecraft.block.material.Material;
  */
 public class BlockTutorialBlock extends Block {
 
+    public static Integer spread;
+    public static Boolean hasSpread;
+
     public BlockTutorialBlock(Material materialIn, String name) {
         super(materialIn);
         this.setUnlocalizedName(name);
         this.setCreativeTab(ModThing.tabTutorial);
-        this.setHardness(50F);
-        this.setResistance(1000F);
+        this.setHardness(5F);
+        this.setResistance(10F);
         this.setSoundType(SoundType.SLIME);
+        this.setLightLevel(1F);
+    }
+
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        spread = rand.nextInt(10000);
+        if (spread <= 5) {
+            if (worldIn.getBlockState(pos).getMaterial() != blockMaterial.AIR) {
+
+            }
+        }
+
+
+        super.updateTick(worldIn, pos, state, rand);
     }
 }
+
